727cc60545448de96f7e979cb7aab3f0e9dcb90e
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-30 20:38:02 +0300

    I have learnt the basics of functions and operators in Javascript

---
+++ functions.js
@@ -0,0 +1,40 @@
+
+
+function happyBirthday(username, age){
+    console.log("Today is my birthday bro!")
+    console.log("Today is my birthday bro!")
+    console.log("Today is my birthday bro!")
+    console.log(`Happy birthday ${username}`)
+    console.log(`You are ${age} years old.`)
+}
+
+happyBirthday("Tendo Calvin",23)
+happyBirthday("David Beckham",50)
+
+function add(x,y){
+    return x + y;
+}
+
+function subtraction(x,y){
+    return x - y;
+}
+
+function multiplication(x,y){
+    return x * y;
+}
+
+function division(x,y){
+    return x / y;
+}
+
+console.log(subtraction(2, 4))
+console.log(add(2, 4))
+console.log(multiplication(2, 4))
+console.log(division(2, 4))
+
+
+function isEven(number){
+    return number % 2 === 0 ? true : false
+}
+
+console.log(isEven(8));
---
+++ operators.js
@@ -0,0 +1,27 @@
+// arithmetic operators(values, variables,)
+//            operators(+, *, -, /)
+
+let students = 30
+//students = students + 2
+// students = students - 2
+// students = students * 2
+//students = students / 3
+// students = students % 2
+
+
+// students +=1
+// students -=1
+//students *=3
+// students **=2
+
+//students--;
+//students++;
+//console.log(students);
+
+/*
+order precedence
+    1. parenthesis
+    2. exponents
+    3. multiplication and division and modulo
+    4. addition and subtraction
+*/
