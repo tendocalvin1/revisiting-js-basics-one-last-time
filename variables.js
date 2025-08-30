2a0f4a9a37472435972ad5ff27306402343b5498
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-30 10:49:12 +0300

    Going through variables in JavaScript programming languange

---
+++ variables.js
@@ -0,0 +1,31 @@
+// Variable = A container that stores a variable
+//            Behaves as if it were the value it contains
+
+// 1. declararion  let x ;
+// assignment x = 1 
+
+let x = 100;
+let age = 23;
+let gpa = 4.3;
+console.log(x);
+console.log(`You are ${age} years old.`);
+console.log(`You scored a gpa of ${gpa}.`);
+
+
+let firstName = "Tendo";
+let lastName = "Arnold";
+
+let FavouriteFood = "pork";
+
+console.log(`My favourite food is ${FavouriteFood}`)
+console.log(typeof lastName)
+
+
+let online = false;
+let forSale = true
+let isStudent = true
+console.log(typeof online);
+console.log(typeof forSale);
+
+console.log(`Is this car for sale: ${forSale}`)
+console.log(`Is this lady a student of UCU: ${isStudent}`)
---
+++ variables2.js
@@ -0,0 +1,11 @@
+
+let age = 23;
+let isStudent = true;
+let fullname = "Tendo Calvin";
+let gpa = 4.3;
+
+
+document.getElementById("p1").textContent = `You have a gpa of ${gpa}`;
+document.getElementById("p2").textContent = `My name is ${fullname}`;
+document.getElementById("p3").textContent = `Are you a student of UCU: ${isStudent}`;
+document.getElementById("p4").textContent = `I am ${age} years old.`;
