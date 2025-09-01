c2a85d440b9682dde2e87e187af9583ca0f97097
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-01 10:40:10 +0300

    I have made a few changes to this file as I have done some simple exercises such as creating a Babk Account as well as creating a secret message

--- closures.js
+++ closures.js
@@ -56,11 +56,109 @@ console.log(addition(4));
 
 // Toggle Switch
 const createToggle =()=>{
-    const lighting =(on)=>{
-        console.log("It is ON")
+
+    let isOn = false;
+
+    const toggle =()=>{
+        isOn = !isOn;
+        return isOn;
     }
-    return lighting;
+
+    return toggle;
 }
 
-const not_lighting = createToggle();
-console.log((not_lighting()));
+const on = createToggle();
+console.log(on());
+
+// secret message
+// Create a function secretMessage(message) that returns two functions:
+// one to reveal the secret,
+// one to update it.
+
+
+const secretMessage = ()=>{
+    let secret = "I am a software engineer." // private, only visible to inner functions
+    const revelation =()=>{
+      return secret;
+    }   
+
+    const update =(new_message)=>{
+        return new_message;
+    }
+
+    // returning both functions
+    return [revelation, update]; // using arrays to return the functions created
+}
+
+// const word = secretMessage()
+// console.log(word[0]()) // invoking the revelation function
+// console.log(word[1]()) // invoking the update function
+
+
+const [revelation , update] = secretMessage("Initial message")
+console.log(revelation()); // this helps to invoke the revelation function
+console.log(update("I work for a startup called Airbnb")); // this helps to invoke the update function
+
+
+
+// secret message
+// Create a function imaginaryMessage(message) that returns two functions:
+// one to show the secret,
+// one to change it.
+
+
+const imaginaryMessage =(message)=>{
+    let talk = "I am an all round software engineer who creates software for the future"
+
+        const show = ()=>{
+            return talk;
+        }
+
+        const change =(message_change)=>{
+            return message_change;
+        }
+
+        // calling or invoking both sets of functions
+        return [show, change]
+
+
+}
+
+const [show, change] = imaginaryMessage("Initial message");
+console.log(show()); // this helps to invoke the show function
+console.log(change("I am a Real Estate developer and a software engineer.")); // this helps to invoke the change function
+
+
+// Private Bank Account
+// Create a createBankAccount(initialBalance) function that returns methods:
+// deposit(amount),
+// withdraw(amount),
+// getBalance().
+// The balance must remain private.
+
+
+const createBankAccount = (initial_deposit)=>{
+    let amount = 200000
+    let amount_withdrawn = 5000
+    let interest = 0.25
+
+    const deposit =()=>{
+        return amount;
+    }
+
+    const withdraw =()=>{
+        return balance = amount - amount_withdrawn;
+    }
+
+    const getBalance =()=>{
+        return balance = deposit - amount_withdrawn - interest
+    }
+
+    return [deposit, withdraw, getBalance]
+}
+
+
+const [deposit, withdraw, getBalance] = createBankAccount("Initial Deposit")
+console.log(deposit()); // invokes the deposit function
+console.log(withdraw()); // invokes the withdraw function
+console.log(getBalance()); // invokes the getBalance function
