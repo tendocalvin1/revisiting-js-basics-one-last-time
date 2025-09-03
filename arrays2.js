92501e6277072687520489cad25e2fafbba3a3c1
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-03 11:58:55 +0300

    Modified this file by adding certain answers and solutions

---
+++ arrays2.js
@@ -0,0 +1,16 @@
+// arrays
+
+let fruits = ['bananas', 'apples','pineapples'];
+// fruits.push('mangoes');
+// console.log(fruits);
+
+// fruits.shift() // removes the element at the beginning
+// console.log(fruits);
+
+// fruits.pop() // removes the element at the end
+// console.log(fruits)
+
+
+for(let fruit of fruits){
+    console.log(fruits)
+}
