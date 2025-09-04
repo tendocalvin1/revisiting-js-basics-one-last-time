7116be1d4e7c94e9835aade7533918c311850a9a
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-04 20:06:50 +0300

    I have added the whole topic structure of DOM manipulation in JavaScript that will enable me to get good at Full-Stack development

---
+++ Javascript-dom.js
@@ -0,0 +1,62 @@
+
+//  1. What is DOM
+// 2. What is nodes and types of nodes 
+
+// 3. Selecting elements using DOM 
+    // (a) getElementById
+    // (b) getElementsByname()
+    // (c) getElementsByTagName() 
+    // (d) getElementsByClassName()
+    // (e) querySelector() and querySelectorAll()
+
+
+// 4. Traversing Elements
+    // (a) selecting parent element
+    // (b) selecting child element
+    // (c)  selecting sibling elements
+
+
+// 5. Manipulating HTML elements
+    // 1. createElement()
+    // 2. appendChild()
+    // 3. textContent
+    // 4. innerHTML
+    // 5. after()
+    // 6. append()
+    // 7. prepend()
+    // 8. insertAdjacentHTML()
+    // 9. replaceChild()
+    // 10. cloneNode()
+    // 11. removeChild()
+    // 12. insertBefore()
+
+
+// 6. Attribute Methods
+    // 1. getAttribute()
+    // 2. setAttribute()
+    // 3. hasAttribute()
+    // 4. removeAttribute()
+
+
+// 7. Manipulating Elements' styles
+    // 1. style property
+    // 2. CSS Text
+    // 3. getComputedStyle()
+    // 4. className property
+    // 5. classList Property
+
+
+// 8. JavaScript Events
+    // 1. What is an Event
+    // 2. Event Bubbling and Event capturing
+    // 3. Event handler in HTML attributes
+    // 4. DOM level 0 event handlers
+    // 5. AddEventListener()
+    // 6. removeEventListener()
+    // 7. Event Objects
+    // 8. Different types of Event
+
+
+// DOM manipulation is an API(Application Programming Interface)for manipulating HTML documents
+
+
